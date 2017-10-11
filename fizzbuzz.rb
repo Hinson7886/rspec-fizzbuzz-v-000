23e06fa2daedd23db6b561f5c5dbd2c674b6c018
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-11 22:48:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(int)
   if int % 5 == 0
-    return "Fizz"
-  elsif int % 3 == 0
     return "Buzz"
+  elsif int % 3 == 0
+    return "Fizz"
   elsif int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
   else
